c8a567646dfab06d6f46ae4f89fb378af703e49d
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-08-30 20:00:58 -0300

    Second Class

--- public/bundle.js
+++ public/bundle.js
@@ -1,3 +1,25 @@
 "use strict";
 
-console.log("Hello");
+// var, const & let
+{
+  // isso é um escopo, abre e fecha chaves
+  var _car = "fusca";
+}
+console.log(car);
+/* vai dar erro pois o const 
+deixa o car isolado no escopo */
+// var, const & let
+
+var person = {
+  name: "Yun"
+};
+person.name = "Olivia";
+console.log(person); // mutabilidade
+// dar preferência sempre ao const por tornar o código mais seguro
+// var, const & let
+
+for (var _index = 0; _index < 10; _index++) {
+  console.log(_index);
+}
+
+console.log(index);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+// var, const & let
+ {// isso é um escopo, abre e fecha chaves
+  const car = "fusca";
+}
+
+console.log(car) /* vai dar erro pois o const 
+deixa o car isolado no escopo */
+
+// var, const & let
+const person = { name: "Yun"};
+
+person.name = "Olivia"
+
+console.log(person)// mutabilidade
+
+// dar preferência sempre ao const por tornar o código mais seguro
+// var, const & let
+for (let index = 0; index < 10; index++) {
+ console.log(index);
+}
+
+console.log(index)
