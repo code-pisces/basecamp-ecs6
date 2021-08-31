ad0bb21a9dd4e13628a26ee964647236b074d568
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-08-31 19:37:05 -0300

    Arrow Functions

--- public/bundle.js
+++ public/bundle.js
@@ -1,18 +1,5 @@
 "use strict";
 
-var numbers = [1, 2, 3, 4, 5];
-numbers.forEach(function (number, index) {
-  numbers[index] = number + 1;
-});
-var newNumbers = numbers.map(function (number) {
-  return number + 1;
-});
-var total = numbers.reduce(function (total, next) {
-  return total + next; // 0 + 1, 1 + 2
-});
-console.log(total);
-console.log(newNumbers);
-console.log(numbers);
 var todos = [{
   text: "Lavar a louça",
   done: false
@@ -27,16 +14,13 @@ var doneTodos = todos.filter(function (todo) {
   return todo.text === "Dobrar as roupas";
 });
 console.log(doneTodos);
-var find = todos.find(function (todo) {
-  return todo.text === "Dobrar as roupas";
-});
-console.log(find);
-var index = todos.findIndex(function (todo) {
-  return todo.text === "Dobrar as roupas";
-});
-console.log(index);
-var cars = ["fusca", "civic", "hb20"];
-var hasFusca = cars.some(function (car) {
-  return car === "fusca";
-});
-console.log(hasFusca);
+/*
+function anything() {
+  console.log(doneTodos);
+}*/
+
+var anything = function anything() {
+  return {
+    name: "Yung"
+  };
+};
--- src/main.js
+++ src/main.js
@@ -1,48 +1,15 @@
-const numbers = [1, 2, 3, 4, 5];
-
-numbers.forEach(function (number, index) {
-  numbers[index] = number + 1;
-});
-
-const newNumbers = numbers.map(function (number) {
-  return number + 1;
-});
-
-const total = numbers.reduce(function (total, next) {
-  return total + next; // 0 + 1, 1 + 2
-});
-
-console.log(total);
-console.log(newNumbers);
-console.log(numbers);
-
 const todos = [
   { text: "Lavar a louça", done: false },
   { text: "Apanhar a lixo", done: true },
   { text: "Dobrar as roupas", done: true },
 ];
 
-const doneTodos = todos.filter(function (todo) {
-  return todo.text === "Dobrar as roupas";
-});
+const doneTodos = todos.filter((todo) => todo.text === "Dobrar as roupas");
 
 console.log(doneTodos);
+/*
+function anything() {
+  console.log(doneTodos);
+}*/
 
-const find = todos.find(function (todo) {
-  return todo.text === "Dobrar as roupas";
-});
-
-console.log(find);
-
-const index = todos.findIndex(function (todo) {
-  return todo.text === "Dobrar as roupas";
-});
-
-console.log(index);
-
-const cars = ["fusca", "civic", "hb20"];
-const hasFusca = cars.some(function (car) {
-  return car === "fusca";
-});
-
-console.log(hasFusca);
+const anything = () => ({ name: "Yung" });
